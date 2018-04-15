a6221c4deb1c50b2e1ebdfca4bb541779256f859
Igor Sosa Mayor <joseleopoldo1792@gmail.com>
2018-04-15 17:12:32 +0200

    Haciendo conexiones, etc.

--- gui/nuevolibroentrada.cpp
+++ gui/nuevolibroentrada.cpp
@@ -6,9 +6,40 @@ NuevoLibroEntrada::NuevoLibroEntrada(QWidget *parent) :
     ui(new Ui::NuevoLibroEntrada)
 {
     ui->setupUi(this);
+
+    connect(ui->btOK, SIGNAL(clicked(bool)), this, SLOT(aceptarLibro()));
+    connect(ui->btAnadirAutor, SIGNAL(clicked(bool)), this, SLOT(anadirAutor()));
+    connect(ui->btQuitarAutor, SIGNAL(clicked(bool)), this, SLOT(quitarAutor()));
+    connect(ui->btAnadirCategoria, SIGNAL(clicked(bool)), this, SLOT(anadirCategoria()));
+    connect(ui->btQuitarCategoria, SIGNAL(clicked(bool)), this, SLOT(quitarCategoria()));
 }
 
 NuevoLibroEntrada::~NuevoLibroEntrada()
 {
     delete ui;
+}
+
+void NuevoLibroEntrada::aceptarLibro()
+{
+
+}
+
+void NuevoLibroEntrada::anadirAutor()
+{
+
+}
+
+void NuevoLibroEntrada::quitarAutor()
+{
+
+}
+
+void NuevoLibroEntrada::anadirCategoria()
+{
+
+}
+
+void NuevoLibroEntrada::quitarCategoria()
+{
+
 }
--- gui/nuevolibroentrada.h
+++ gui/nuevolibroentrada.h
@@ -15,8 +15,28 @@ public:
     explicit NuevoLibroEntrada(QWidget *parent = 0);
     ~NuevoLibroEntrada();
 
+private slots:
+
+    void aceptarLibro();
+    void anadirAutor();
+    void quitarAutor();
+    void anadirCategoria();
+    void quitarCategoria();
+
+signals:
+
+    void anadirAutorSignal();
+
 private:
     Ui::NuevoLibroEntrada *ui;
+
+    struct elementopareado{
+      int id = 0;
+      QString elemento = "";
+    };
+
+    QList<elementopareado> autores;
+    QList<elementopareado> categorias;
 };
 
 #endif // NUEVOLIBROENTRADA_H
